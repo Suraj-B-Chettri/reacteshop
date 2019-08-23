6794f50b9b36d8136fa51d321e88ca9c492ae270
Suraj-B-Chettri <suraj.bohara.589@gmail.com>
2019-08-23 16:13:39 +0530

    ee

--- src/features/product-listing/index.js
+++ src/features/product-listing/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import ProductListItem from './product-list-item';
 
-export default function ProductListing(props) {
+ function ProductListing(props) {
     return <div className="product-listing">
         {
             props.products.map(product =>
                 <ProductListItem key={product.ProductId} product = {product}/>)
         }
     </div>
+}
+
+
+function mapStateToProps(state) {
+    return {
+        cart: state.cart
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        addToCart: (item) =>{
+            dispatch({type:'Add', payload: item})
+        },
+        removeFromCart: (item) =>{
+            dispatch({type:'REMOVE', payload: item})
+        }
+
+    }
 }
--- src/features/product-listing/product-list-item.js
+++ src/features/product-listing/product-list-item.js
@@ -2,14 +2,15 @@ import React from 'react'
 // src={`/products/${props.products.productProductPicUrl}`}
 export default function ProductListItem(props) {
     return <div className="product-list-item">
-        <h3>{props.Name}</h3>
+        <h3>{props.product.Name}</h3>
         <img
         height ={100}
         title ={props.product.Name}
         src={props.product.ProductPicUrl}
- />
-        <div>{props.product.Description}</div>
-        <div>{props.product.Price}</div>
+        alt={props.product.Name}
+        />
+        <div>Description: {props.product.Description}</div>
+        <div>Price: ${props.product.Price}</div>
         <div>
             <button>Add to Cart</button>
         </div>
